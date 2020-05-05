17f8211fb4fd605af4cc03c10df9fe380d78dd52
Jarrod Miller <jarrod.miller@activeprospect.com>
2020-05-04 22:16:15 -0500

    Added some kinda shite practice session functionality

---
+++ src/assets/questions/pemdas.js
@@ -0,0 +1,34 @@
+const questions = [
+ {
+  q: '3+3+3',
+  a: '9',
+  solved: false,
+  weight: 5
+ },
+ {
+  q: '3+3+3+3',
+  a: '12',
+  solved: false,
+  weight: 5
+ },
+ {
+  q: '3+3+4',
+  a: '10',
+  solved: false,
+  weight: 5
+ },
+ {
+  q: '3+3+3+1',
+  a: '10',
+  solved: false,
+  weight: 5
+ },
+ {
+  q: '3+3+2',
+  a: '8',
+  solved: false,
+  weight: 5
+ },
+];
+
+module.exports = questions;
--- src/components/Pemdas/Solve.vue
+++ src/components/Pemdas/Solve.vue
@@ -1,12 +1,62 @@
 <template>
   <div>
-    Solve
+    {{ this.currentQuestion.q }}
+    <input v-model="answer" placeholder="Your Answer Here">
+    <button @click="submit">Submit</button>
+    <p>{{ this.message }}</p>
+    <p>Timer: {{ this.timer }}</p>
+    <p>Score: {{ this.score }}</p>
   </div>
 </template>
 
 <script>
+  import questions from '../../assets/questions/pemdas';
   export default {
-    
+    data() {
+      return {
+        questions,
+        index: 0,
+        answer: '',
+        score: 0,
+        timer: 0,
+        message: '',
+        intervalId: null,
+      }
+    },
+    computed: {
+      currentQuestion: function() {
+        return this.questions[this.index]
+      },
+    },
+    methods: {
+      submit: function() {
+        if (this.answer !== this.currentQuestion.a) {
+          this.message = 'Wrong! Keep Trying';
+        } else {
+          this.message = '';
+          this.score += this.timer - this.currentQuestion.weight;
+          this.timer = 0;
+          this.answer = '';
+          this.index += 1;
+          this.clearTimer();
+          this.startTimer();
+        }
+      },
+      startTimer: function() {
+        const id = setInterval(() => {
+          console.log('Hello!');
+          this.timer += 1;
+        }, 1000);
+        this.intervalId = id;
+      },
+      clearTimer: function() {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+      }
+    },
+    mounted() {
+      this.startTimer();
+    }
   }
 </script>
 
